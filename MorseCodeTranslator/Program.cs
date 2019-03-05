65aee3527f2389401f106bc298231b0dedae97fa
jeffreyhamilton <jvhamilton85@gmail.com>
2019-03-04 20:01:33 -0500

    Finished project.

--- MorseCodeTranslator/Program.cs
+++ MorseCodeTranslator/Program.cs
@@ -10,6 +10,23 @@ namespace MorseCodeTranslator
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+
+
+                Console.Write(": ");
+                string input = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    break;
+                }
+
+                string output = MorseCodeTranslator.ToMorse(input);
+
+                Console.WriteLine(output);
+            }
+
         }
     }
 }
